31facf22895977da8b46dfe461fedda94c0ea812
Jakubstaniszewski <s096660@student.tu.kielce.pl>
2025-06-06 17:15:31 +0200

    adding db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const app = express();
+const { Pool } = require("pg");
+require("dotenv").config();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
+
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Hello from backend! PostgreSQL time: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Błąd połączenia z bazą danych");
+  }
 });
 
 app.listen(PORT, () => {
